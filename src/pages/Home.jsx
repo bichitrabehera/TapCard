d67de43345b4e0257ae8658f102ca553481ffb9d
bichitrabehera <bichitrabehera.amcec@gmail.com>
2025-07-19 20:09:15 +0530

    design

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import Navbar from '../Components/navbar'
+import Navbar from '../Components/Navbar'
 import Header from '../Components/Header'
 import AboutUs from '../Components/AboutUs'
 import WhyChooseUs from '../Components/WhyUs'
